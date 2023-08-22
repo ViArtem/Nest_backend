9251b6e4c7956a7b301569a4dc9849f77fe02374
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-08-22 14:55:38 +0300

    added order db model and eddited module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from 'src/users/users.model';
+import { Order } from './order.model';
 
 @Module({
-  providers: [OrderService],
-  controllers: [OrderController]
+  controllers: [OrderController],
+  providers: [
+    OrderService,
+    {
+      provide: "ORDER_REPOSITORY", // Ім'я провайдера
+      useValue: Order, // Клас моделі
+    },
+  ],
+  imports: [SequelizeModule.forFeature([User, Order])],
 })
 export class OrderModule {}
