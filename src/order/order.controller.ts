6a127452cd852121bd10f335c60881a8386a5c87
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-08-22 15:08:40 +0300

    added create order route

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Put,
+  Body,
+  Param,
+} from "@nestjs/common";
+import { OrderService } from "./order.service";
+import { CreateOrderDto } from "./dto/create-order.dto";
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Post("create")
+  create(@Body() CreateDto: CreateOrderDto) {
+    return this.orderService.create(CreateDto);
+  }
+}
